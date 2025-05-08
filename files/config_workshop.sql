0969047d4a415b2b22e78db797741bc4b5b8d0b2
Inge Os <inge.os@oracle.com>
2025-05-08 19:10:44 +0200

    Update config_workshop.sql
    Removed initial password

--- files/config_workshop.sql
+++ files/config_workshop.sql
@@ -4,12 +4,12 @@ create or replace package lab_config as
 --  Package that configures the PL/SQL creating the workshop
 --  Run prior to user creation or workspace creation, set defaults
 --  (c) Inge Os 2024
---  29.08.24
+--  08.05.25
 --
 --
-    base_username varchar2(15):='';
-    lab_password varchar2(20):='';
-    initial_password varchar2(20):='';
+    base_username varchar2(15):='labsomething';
+    lab_password varchar2(20):='apassword';
+    -- initial_password varchar2(20):='';
     user_count number:=10;  --  1= no number added,create just the base 
 end lab_config;
 /
